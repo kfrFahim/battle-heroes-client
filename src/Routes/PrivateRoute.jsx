c6b9376610d7571b3203b7b4ce1d246a2f4b2420
kfrFahim <kfrfahim3@gmail.com>
2023-05-19 17:39:01 +0600

    private route

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useContext } from 'react';
+import { AuthContext } from '../providers/AuthProvider';
+import { Navigate } from 'react-router';
+
+
+const PrivateRoute = () => {
+     return (
+          <div>
+               
+          </div>
+     );
+};
+
+export default PrivateRoute;
