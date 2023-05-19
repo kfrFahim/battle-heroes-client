af7f67e0c70dc3b3acd1667eebb6f5d61a4f1cf7
kfrFahim <kfrfahim3@gmail.com>
2023-05-19 16:48:40 +0600

    my jobs section

--- src/pages/AddToy/AddToy.jsx
+++ src/pages/AddToy/AddToy.jsx
@@ -1,39 +1,56 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useForm } from "react-hook-form";
+import { AuthContext } from '../../Providers/AuthProvider';
 
 const AddToy = () => {
 
+     const {user} = useContext(AuthContext);
+
      const { register, handleSubmit, watch, formState: { errors } } = useForm();
-     const onSubmit = data => console.log(data);
+
+
+     const onSubmit = data => {
+          fetch("http://localhost:5000/addtoy",{
+               method:"POST",
+               headers: {
+                    "content-type" : "application/json"
+               },
+               body: JSON.stringify(data)
+          })
+          .then(res => res.json())
+          .then(result => console.log(result) )
+          
+          console.log(data);
+     };
 
      return (
           <div className='max-w-[1200px] mx-auto'>
                <h1>Add a toy</h1>
 
-               <div className='border border-gray-600 p-5'>
+               <div className='shadow-xl p-5 my-8'>
                <form  onSubmit={handleSubmit(onSubmit)}>
       {/* register your input into the hook by invoking the "register" function */}
 <div className='grid grid-cols md:grid-cols-2 gap-5'>
-<input className='border border-black p-3' placeholder='Name' {...register("name")} />
+<input className='text-black font-semibold rounded shadow border border-gray-400 bg-gray-200  p-3' placeholder='Name' {...register("name")} />
       
-      <input className='border border-black p-3' placeholder='Seller Name' defaultValue="" {...register("seller-name")} />
+      <input className='text-black font-semibold rounded shadow border border-gray-400 bg-gray-200  p-3' value={user?.name} placeholder='Seller Name' defaultValue="" {...register("seller-name")} />
 
-      <input className='border border-black p-3' placeholder='Photo URL' defaultValue="" {...register("photo-url")} />
+      <input className='text-black font-semibold rounded shadow border border-gray-400 bg-gray-200  p-3' placeholder='Photo URL' defaultValue="" {...register("photo-url")} />
       
       {/* include validation with required or other standard HTML validation rules */}
-      <input type='email' className='border border-black p-3' placeholder='Email'   {...register("email", { required: true })} />
+      <input type='email' value={user?.email} className='text-black font-semibold rounded shadow border border-gray-400 bg-gray-200  p-3' placeholder='Email'   {...register("posted-by", { required: true })} />
 
-      <select className='border border-black' placeholder='Toy Category' {...register("toy-category")}>
-        <option value="female">Avengers</option>
-        <option value="male">Marvel</option>
-        <option value="other">Transformer</option>
-        <option value="other">Star Wars</option>
+      <select className='text-black font-semibold rounded shadow border border-gray-400 bg-gray-200  p-3' placeholder='Toy Category' {...register("toy-category")}>
+        <option value="avengers">Avengers</option>
+        <option value="marvel">Marvel</option>
+        <option value="transformer">Transformer</option>
+        <option value="star-wars">Star Wars</option>
       </select>
 
-      <input className='border border-black p-3' placeholder='Price' defaultValue="" {...register("price")} />    
-      <input className='border border-black p-3' placeholder='Rating' defaultValue="" {...register("rating")} />    
-      <input className='border border-black p-3' placeholder='Available quantity' defaultValue="" {...register("quantity")} /> 
-      <input className='border border-black p-3' placeholder='Description' defaultValue="" {...register("description")} /> 
+      <input className='text-black font-semibold rounded shadow border border-gray-400 bg-gray-200 p-3' placeholder='Price' defaultValue="" {...register("price")} />    
+      <input className='text-black font-semibold rounded shadow border border-gray-400 bg-gray-200  p-3' placeholder='Rating' defaultValue="" {...register("rating")} />    
+      <input className='text-black font-semibold rounded shadow border border-gray-400 bg-gray-200  p-3' placeholder='Available quantity' defaultValue="" {...register("quantity")} /> 
+      <input className='text-black font-semibold rounded shadow border border-gray-400 bg-gray-200  px-3 py-5' placeholder='Description' defaultValue="" {...register("description")} /> 
 
 
 
@@ -45,7 +62,9 @@ const AddToy = () => {
 
 
       
-      <input className='btn btn-block bg-blue-300 p-4 my-6' type="submit" />
+      <div className='flex justify-center'>
+      <input className='btn bg-blue-300 text-black px-8 py-3 my-6' type="submit" />
+      </div>
     </form>
                </div>
           </div>
