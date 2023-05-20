2d9fd00778ea346232714413854fd60b257cb82c
kfrFahim <kfrfahim3@gmail.com>
2023-05-20 16:09:51 +0600

    footer logo

---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="max-w-[1200px] mx-auto">
+      <div>
+        <div className="flex gap-3 items-center">
+          <img
+            className="w-12 h-12 rounded-full"
+            src="https://pbs.twimg.com/profile_images/1466072342816243718/9A_DD3bJ_400x400.png"
+            alt=""
+          />
+          <p className="text-xl font-bold">BattleHeroes</p>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
